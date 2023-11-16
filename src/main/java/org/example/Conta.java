c2458d815659b1544247fe0101fd73df78adb4b2
Mariana Sauer <marianasauer2004@gmail.com>
2023-11-16 14:33:56 -0300

    Exceções para não aceitas campos nulos ao criar conta

--- src/main/java/org/example/Conta.java
+++ src/main/java/org/example/Conta.java
@@ -1,11 +1,14 @@
 package org.example;
-
+import Exception.ValidationException;
 public class Conta {
     private Long id;
     private String nome;
     private Usuario usuario;
 
     public Conta(Long id, String nome, Usuario usuario) {
+        if (nome == null) throw new ValidationException("Nome é obrigatório");
+        if (usuario == null) throw new ValidationException("Usuário é obrigatório");
+
         this.id = id;
         this.nome = nome;
         this.usuario = usuario;
