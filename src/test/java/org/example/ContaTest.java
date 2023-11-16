4144f931170de19d7da1a61744e9c184f035ee28
Mariana Sauer <marianasauer2004@gmail.com>
2023-11-16 14:32:42 -0300

    Implementando o método para não validar conta com campos vazios e utilizando o @MethodSource para prover dados junto com o Objeto Usuário criado

--- src/test/java/org/example/ContaTest.java
+++ src/test/java/org/example/ContaTest.java
@@ -4,16 +4,41 @@ import builders.ContaBuilder;
 import builders.UsuarioBuilder;
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import Exception.ValidationException;
+import org.junit.jupiter.params.provider.Arguments;
+import org.junit.jupiter.params.provider.MethodSource;
+
+import java.util.stream.Stream;
+
+import static builders.ContaBuilder.umaConta;
 
 public class ContaTest {
     @Test
     public void deveCriarContaValida(){
         //Criar uma conta
-        Conta conta = ContaBuilder.umaConta().agora();
+        Conta conta = umaConta().agora();
         //Assertivas em cima da conta
         Assertions.assertAll("Conta",
                 () -> Assertions.assertEquals(1L, conta.id()),
                 () -> Assertions.assertEquals("Conta Válida", conta.nome()),
                 () -> Assertions.assertEquals(UsuarioBuilder.umUsuario().agora(), conta.usuario()));
     }
-}
+
+    @ParameterizedTest
+    @MethodSource(value = "dataProvider")
+    public void deveRejeitarContaInvalida(Long id, String nome, Usuario usuario, String mensagem){
+        String errorMessage = Assertions.assertThrows(ValidationException.class, () ->
+                umaConta().comId(id).comNome(nome).comUsuario(usuario).agora()).getMessage();
+        Assertions.assertEquals(mensagem, errorMessage);
+    }
+
+    private static Stream<Arguments> dataProvider() {
+        return Stream.of(
+                Arguments.of(1L, null, UsuarioBuilder.umUsuario().agora(), "Nome é obrigatório"),
+                Arguments.of(1L, "Conta Válida", null, "Usuário é obrigatório")
+
+                );
+    }
+
+    }
