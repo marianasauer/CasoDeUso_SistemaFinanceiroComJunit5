fe1338b963432434bae3e80a59ea265e8cc81d63
Mariana Sauer <marianasauer2004@gmail.com>
2023-11-16 15:12:41 -0300

    Criando a Camada de Serviço

---
+++ src/main/java/services/UsuarioService.java
@@ -0,0 +1,13 @@
+package services;
+
+import domain.Usuario;
+
+public class UsuarioService {
+    public Usuario salvar(Usuario usuario){
+        //Conectar com o banco
+        //Preparar o SQL
+        //Executar a consulta
+        //Obter o usuário persistido
+        return usuario;
+    }
+}
